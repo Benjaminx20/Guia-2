2010864521d4555460d4b79471812eb4b57bfe00
Benjaminx20 <miguelmrtinez219@gmail.com>
2025-09-19 09:19:36 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,48 +1,60 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ejercicio5
+namespace Ejercicio4
 {
     internal class Program
     {
         static void Main(string[] args)
-        {        
-            ArrayList cantidades = new ArrayList();    
-            List<string> palabras = new List<string>()
-        {
-            "PROGRAMAR",
-            "FUTBOL",
-            "GUITARRA",
-            "ROCK"
-        };
+    
 
-          
-            foreach (string palabra in palabras)
             {
-                cantidades.Add(palabra.Length);
-            }
+               
+                List<int> numeros = new List<int>() { 12, 45, 7, 33, 21 };
 
-            Console.WriteLine("Palabras en la lista:");
-            foreach (string palabra in palabras)
-            {
-                Console.WriteLine("- " + palabra);
-            }
+                Console.WriteLine("Lista de números:");
+                for (int i = 0; i < numeros.Count; i++)
+                {
+                    Console.WriteLine($"Posición {i} = {numeros[i]}");
+                }
 
-            Console.WriteLine("\nCantidad de letras en cada palabra:");
-            foreach (int cantidad in cantidades)
-            {
-                Console.WriteLine("- " + cantidad);
-            }
+               
+                int mayor = numeros[0];
+                int posMayor = 0;
+
+            
+                int menor = numeros[0];
+                int posMenor = 0;
+
+                for (int i = 1; i < numeros.Count; i++)
+                {
+                    if (numeros[i] > mayor)
+                    {
+                        mayor = numeros[i];
+                        posMayor = i;
+                    }
+                    if (numeros[i] < menor)
+                    {
+                        menor = numeros[i];
+                        posMenor = i;
+                    }
+                }
+
+             
+                int medio = numeros[2];
+                int posMedio = 2;
+
+                Console.WriteLine($"\nNúmero mayor: {mayor}, en la posición {posMayor}");
+                Console.WriteLine($"Número del medio: {medio}, en la posición {posMedio}");
+                Console.WriteLine($"Número menor: {menor}, en la posición {posMenor}");
 
             Console.ReadKey();
         }
+        
+        }
+
     }
 
-
-
-}
-   
